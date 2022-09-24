ffda05f5da4ae6a68b937c4446b795d0279d934d
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-24 18:38:41 +0530

    let vs var

---
+++ 1.Intro/4.let_const_var.js
@@ -0,0 +1,14 @@
+console.log("JS Tutorial on Let, var,const");
+
+var a = 45;
+var b = 'Abhi';
+var c = null;
+var d = undefined;
+
+
+{
+	let b = "Bisht"
+	console.log("Inside the block value of b: ",b);
+}
+
+console.log("Global Scope value of b: ",b);
