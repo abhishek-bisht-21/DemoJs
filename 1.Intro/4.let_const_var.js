2135b278d28d28d445f32ba8c38fcdb584013bbb
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-24 18:48:52 +0530

    Pointers about let,var and const

--- 1.Intro/4.let_const_var.js
+++ 1.Intro/4.let_const_var.js
@@ -1,6 +1,7 @@
 console.log("JS Tutorial on Let, var,const");
 
 // Var can be redeclared, whereas let can't be.
+// Var has global scope and can be updated and redeclared
 var a = 45;
 var a = null;
 
@@ -8,7 +9,11 @@ var b = 'Abhi';
 var c = null;
 var d = undefined;
 
-const author = "Abhishek";
+// Let can be updated but can't be redeclared
+let k = "Abhi"
+let k = "Shake" // Throws error
+
+const author = "Abhishek"; // Const must be initialised when they are declared unlike let and var
 let author = "abc"; // Throws error bcoz const variable can't changed
 
 
