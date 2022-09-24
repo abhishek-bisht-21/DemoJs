ec465932c211eb014875510f5f1b1290d3b0de8e
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-24 20:38:07 +0530

    Post and Pre Inc/dec Operator

--- 1.Intro/6.Operators&Expression.js
+++ 1.Intro/6.Operators&Expression.js
@@ -9,6 +9,17 @@ console.log("a / b: ", a/b);
 console.log("a % b: ", a%b);
 console.log("a ** b: ", a**b);
 
+console.log("a++ :", a++);
+console.log("After Incrementing a: ", a);
+console.log("a--: ", a--);
+console.log("After Decrementing b: ", b);
+
+console.log("Pre Increment ++b: ", ++b)
+console.log("Pre Decrement ++b: ", --b)
+
+
+
+
 
 
 
