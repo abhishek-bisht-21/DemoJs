6887c935a9fb765787c56b089d9647c39b966516
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-24 20:43:04 +0530

    Assignment OP

--- 1.Intro/6.Operators&Expression.js
+++ 1.Intro/6.Operators&Expression.js
@@ -18,6 +18,18 @@ console.log("Pre Increment ++b: ", ++b)
 console.log("Pre Decrement ++b: ", --b)
 
 
+// Assignment Operator
+let x = 10
+console.log("x+=10 : ",x+=10); // x = 20
+console.log("x-=10 : ",x-=10); // x = 10
+console.log("x*=10 : ",x*=10); // x = 100
+console.log("x/=10 : ",x/=10); // x = 10
+
+
+
+
+
+
 
 
 
