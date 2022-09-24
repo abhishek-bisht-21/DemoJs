b753eecbd430cefef902030f945ef1b4eb38b033
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-24 19:01:36 +0530

    Primitive Datatype in Js

---
+++ 1.Intro/5.PrimitiveTypesInJS.js
@@ -0,0 +1,17 @@
+let a = null
+let b = 21
+let c = true;
+let d = "string"
+let e = undefined
+let f = Symbol("Nice Symbol")
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+
+
+
+
