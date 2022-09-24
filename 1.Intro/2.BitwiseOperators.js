f6009c6af4d0a4bcbc06a97453639c4ebd2a4fb7
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-24 18:11:25 +0530

    Intro to Js

---
+++ 1.Intro/2.BitwiseOperators.js
@@ -0,0 +1,15 @@
+
+
+console.log(5 & 3);
+console.log(8 | 1);
+console.log(4 << 1); // Increases the Power of 2 by 1
+console.log(4 >> 1); // Decrease the power of 2 by 1
+console.log(3 ^ 3);
+
+
+// String Concatenation
+console.log("Abhishek" + "Bisht");
+
+
+
+
---
+++ 1.Intro/3.Conditionals.js
@@ -0,0 +1,6 @@
+
+if(10 < 20){
+	console.log("Condition is True");
+}else{
+	console.log("Condition is False");
+}
