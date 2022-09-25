c198157e160a52c0adaf22ebcdd1ed4a89d53f26
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-09-25 11:37:22 +0530

    Arrow function

---
+++ 1.Intro/8.Functions.js
@@ -0,0 +1,23 @@
+
+
+
+function calAverage(a,b){
+	return (a+b)/2;
+}
+
+
+
+
+let a = 10;
+let b = 5;
+
+
+let ans  = calAverage(a,b);
+
+console.log("Average of "+a+" and "+b+" is "+ans )
+
+const sum = (p,q) => { 
+	return p+q;
+}
+
+console.log("Sum of "+a+" and "+b+" is "+sum(a,b))
