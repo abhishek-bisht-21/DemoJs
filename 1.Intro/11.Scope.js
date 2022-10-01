46989ccbe928d8e6a0d2463ae2ee8355fa8ed182
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-10-01 14:56:05 +0530

    Info about var

--- 1.Intro/11.Scope.js
+++ 1.Intro/11.Scope.js
@@ -1,19 +1,34 @@
+// 1) How to initialise functional, global or block scope in JS.
+// 2) 
 {
 	let x = "Hello";
 	console.log(x);
-	var a = 10
+	var a = 10; // becomes global because of var
 	{
 		console.log(x)
 	}
 }
 
+// console.log(x) -> Throw an error  
 console.log(a)
 
 function fun(){
 	console.log(x); // Why we get undefined is lexical scoping
-	var x = 20
+	var x = 20;
 	console.log(x);
 
 }
+
+if(true){
+	var z = 50;
+}
+
+if(false){
+	var y = 60;
+}
+
+console.log(z);
+console.log(y);
+
 
 fun();
