e59e7199941890dd82c62d09812d30179f028954
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-10-02 23:45:45 +0530

    Starting out with coercion

--- 2.Coercion/1.Intro_to_coercion.js
+++ 2.Coercion/1.Intro_to_coercion.js
@@ -1,5 +1,7 @@
 // Coercion stands for type interconversion.
 // There are two types of type interconversion - > 1) Implicit 2) Explicit
+// 1) Implicit -> Language converts the type on its own.
+// 2) Explicit -> You manually convert the type of the something
 console.log("1" + 1);
 console.log(1 - "1");
 console.log("1" + "2");
