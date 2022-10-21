e3bad6f87e7bffc4ae84d64097b02e9c2b05f7d7
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-10-21 10:20:33 +0530

    Callbacks in jS

---
+++ 3.Clousre&Callbacks/CallBacksDemo.js
@@ -0,0 +1,17 @@
+function fun(x, fn){
+	/*
+		x -> Number
+		fn -> callback function
+	*/
+
+	// Some logic
+	for(let i=0;i<x;i++){
+		console.log(i);
+	}
+
+	fn(); // Calling the callback function passed 
+}
+
+fun(10, function log(){
+	console.log("Custom Logger")
+})
