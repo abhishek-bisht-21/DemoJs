19f48b8bfca38f1f958055b6b2b65e1f9d769f37
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-10-21 17:18:22 +0530

    clearTimeout

--- 3.Clousre&Callbacks/setTimeOutDemo.js
+++ 3.Clousre&Callbacks/setTimeOutDemo.js
@@ -1,5 +1,16 @@
-console.warn("Start")	
-setTimeout(function execute(){
-	console.log("Task Completed After",3000, "ms");
+console.warn("Start Task 1")	
+
+let id1 = setTimeout(function execute(){
+	console.log("Task 1 Completed After",10000, "ms");
+}, 10000)
+console.warn("End Task 1")
+
+console.warn("Start Task 2")	
+
+let id2 = setTimeout(function execute(){
+	console.log("Task 2 Completed After",3000, "ms");
+	clearTimeout(id1);
 }, 3000)
-console.warn("End")	
+console.warn("End Task 2")
+
+	
