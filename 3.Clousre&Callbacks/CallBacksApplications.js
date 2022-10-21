0a805cbeb9c590edff9bbc5a720baa58ca9a6371
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-10-21 10:41:22 +0530

    Map Function in Array

--- 3.Clousre&Callbacks/CallBacksApplications.js
+++ 3.Clousre&Callbacks/CallBacksApplications.js
@@ -4,3 +4,12 @@ arr.map(function process(value,index){  // Argument to this map function expects
 	console.log(value,index);
 });
 
+let x = arr.map(function process(value,index){  // Map function returns a new array
+	console.log(value,index);
+	// On every element the map function calls this process function, and whatever task we perform inside
+	// the callback function(process) it sends it into a new array.
+	return value * value; 
+});
+
+console.log(x);
+console.log(arr);
