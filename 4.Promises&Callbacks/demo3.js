ed27e9feb7d07cd6aa2a32a91dcf1be712d21ff8
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-11-05 20:00:57 +0530

    demo3

--- 4.Promises&Callbacks/demo3.js
+++ 4.Promises&Callbacks/demo3.js
@@ -1,3 +1,6 @@
+// variables persist in the memory and hence we are able to use it later on.
+// function b is executed after function a is already executed and still
+// function b is able to access "name" variable which has the lexical scope of a.
 function a(name){
 	return function b(){
 		console.log(name);
