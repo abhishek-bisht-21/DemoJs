9cd311aa50b2067ef2099f2178482caa0f5ad6e2
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-11-12 23:10:53 +0530

    using callbacks to mimic some functionalities"

--- 6.Promises/callbacksDemo1.js
+++ 6.Promises/callbacksDemo1.js
@@ -1,3 +1,7 @@
+// Tasks:
+// 1. Write function to download data from a url
+// 2. Write a function to save that downlaoded data in a file and return filename
+// 3. Write a function to upload the file written in the previous step to a new url
 
 function fetchCustom(url, fn){ // we are mimicing the function
 	// Download the content of the URL
@@ -10,14 +14,31 @@ function fetchCustom(url, fn){ // we are mimicing the function
 		let response = "Dummy Data";
 		fn(response);
 		console.log("Ending the function")
-	},1000)
+	},1000);
 }
 
-// Tasks:
-// 1. Write function to download data from a url
-// 2. Write a function to save that downlaoded data in a file and return filename
-// 3. Write a function to upload the file written in the previous step to a new url
 
+function writeFile(data, fn){
+	console.log("Started Writing data", data);
+
+	setTimeout(function process(){
+		console.log("Completed Writing data");
+		let filename = "output.txt";
+		fn(filename);
+		console.log("Writing Ended");
+	}, 5000);
+}
+
+
+function uploadFile(filename, newurl,fn){
+	console.log("Upload Started");
+	setTimeout(function process(){
+		console.log("File", filename, "Upload Successfully on", newurl);
+		let uploadResponse = "SUCCESS";
+		fn(uploadResponse);
+		console.log("Upload Ended");
+	},2000);
+}
 
 let response = fetchCustom("wwww.google.com", function downloadCallback(response){
 	console.log("Downloaded response is",response);
