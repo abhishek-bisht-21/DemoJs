52ffceaaecdf07e78e8aaea59332f2f729d26db4
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-11-12 22:56:20 +0530

    Callback function and Returning from it

--- 6.Promises/callbacksDemo1.js
+++ 6.Promises/callbacksDemo1.js
@@ -4,16 +4,23 @@ function fetchCustom(url, fn){ // we are mimicing the function
 	// this downloading can take sometime,
 	// we will download the content from the url, and whatever will be the result, we wil pass to callback
 
-	console.log("Starting Download");
+	console.log("Starting Downloading from", url);
 	setTimeout(function process(){
 		console.log("Download Completed");
 		let response = "Dummy Data";
 		fn(response);
+		console.log("Ending the function")
 	},1000)
 }
 
+// Tasks:
+// 1. Write function to download data from a url
+// 2. Write a function to save that downlaoded data in a file and return filename
+// 3. Write a function to upload the file written in the previous step to a new url
+
+
 let response = fetchCustom("wwww.google.com", function downloadCallback(response){
-	console.log(response);
+	console.log("Downloaded response is",response);
 });
 
 
