bbeabb6b370b77ef4cf70be0be4d6c4539aa9a6b
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-11-17 19:12:36 +0530

    Globals -> Module

---
+++ 7.Intro_to_Node/index.js
@@ -0,0 +1,5 @@
+const searching = require('./searching.js');
+console.log("Start");
+let arr = [5,6,2,1,4,7,8,9];
+let x = 6;
+console.log(searching);
---
+++ 7.Intro_to_Node/searching.js
@@ -0,0 +1,35 @@
+function linearSearching(arr,x){
+	for(let i=0;i<arr.length;i++){
+		if(arr[i] === x){
+			return i;
+		}
+	}
+
+	return NaN;
+}
+
+function binarySearch(arr,x){
+
+	let start = 0;
+	let end = arr.length - 1;
+	
+
+	while(start <= end){
+		let mid = Math.floor((start + end) / 2);
+		if(arr[mid] === x){
+			return mid;
+		}
+		else if(arr[mid] < x){
+			start = mid+1;
+		}else{
+			end = mid-1;
+		}
+	}
+
+	return NaN;
+}
+
+module.exports = {
+	linear : linearSearching,
+	binary : binarySearch
+}
