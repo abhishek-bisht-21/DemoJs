65b9b817e5e826f520c7f7ad427df46cce72de39
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-11-27 15:53:14 +0530

    Different Types of Functions

--- 1.Intro/8.Functions.js
+++ 1.Intro/8.Functions.js
@@ -1,6 +1,7 @@
+// We can return anything from a javascript function. At a single instance depending on the condtion or use
+// case we can return different types of JS values from the function (Ex using if-else) in functions.
 
-
-
+// Function Statement
 function calAverage(a,b){
 	return (a+b)/2;
 }
@@ -19,3 +20,14 @@ const sum = (p,q) => {
 }
 
 console.log("Sum of "+a+" and "+b+" is "+sum(a,b))
+
+
+// Function are First Class Citizens, simply means that Functions can be treated as variables.
+// Function Expression -> Assignment of a function in a variable is possible.
+let a = [1,2,3,4,5]; // Array can also be assigned in a variable.
+let b = a;
+console.log(b);
+
+let funcContainer = function fn(){
+	console.log("I am an Expression");
+}
