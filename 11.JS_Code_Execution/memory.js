3dfae4a624da7779b37ec258488fe82270eab66c
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-02 06:55:47 +0530

    Global and This

---
+++ 11.JS_Code_Execution/memory.js
@@ -0,0 +1,7 @@
+// By default JS Execution context gives us these 2 things.
+// This browser this is by default equal to window object.
+// Code which is outside the any function in the javascript that is called as global code.
+// function  inside ❌ -> global area
+
+console.log(global);
+console.log(this);
