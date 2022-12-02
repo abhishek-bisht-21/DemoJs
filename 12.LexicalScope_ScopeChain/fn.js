78621dfc7e94318e6ec05534f4e16817b2c424e3
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-02 08:42:48 +0530

    Scope Chain

---
+++ 12.LexicalScope_ScopeChain/fn.js
@@ -0,0 +1,17 @@
+console.log("line number 1", varname);
+var varname = 10;
+console.log("line number 3", varname);
+function fn(){
+	console.log("line number 5",varname);
+	var varname = 20;
+	function b(){
+		console.log("line number 8",varname);
+	}
+	b();
+}
+fn();
+console.log(varname);
+
+// Functions are allocated memory in the heap.
+// With every function call a new execution context is created and whole process of 
+// memory allocation phase and code execution phase takes place.
