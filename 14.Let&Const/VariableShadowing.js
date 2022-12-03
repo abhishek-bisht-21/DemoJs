89c4b419fa86f063bbe766aa0158f241b07dc3ae
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-03 10:26:23 +0530

    Variable Shadowing

---
+++ 14.Let&Const/VariableShadowing.js
@@ -0,0 +1,48 @@
+// Variable Shadowing
+
+// Legal Variable Shadowing
+
+// 1
+var fruit1 = "apple";
+console.log(fruit);
+{	// Inner Variable inside the block, Shadows the outer fruit variable
+	let fruit1;
+	console.log(fruit1);
+	fruit1 = "orange";
+	console.log(fruit1);
+}
+console.log(fruit1)
+
+// 2
+let fruit2 = "apple";
+console.log(fruit2);
+{	// Inner Variable inside the block, Shadows the outer fruit variable
+	let fruit2;
+	console.log(fruit2);
+	fruit2 = "orange";
+	console.log(fruit2);
+}
+console.log(fruit2)
+
+// 3
+const fruit3 = "apple";
+console.log(fruit3);
+{	// Inner Variable inside the block, Shadows the outer fruit variable
+	let fruit3;
+	console.log(fruit3);
+	fruit3 = "orange";
+	console.log(fruit3);
+}
+console.log(fruit3)
+
+
+
+// Illegal Shadowing
+let fruit = "apple";
+console.log(fruit);
+{	// Inner Variable inside the block, Shadows the outer fruit variable
+	var fruit = "orange";
+	console.log(fruit);
+	
+}
+console.log(fruit)
