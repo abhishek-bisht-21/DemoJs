2e9c3ae2fc6d8ffaa178197c8c5800213b11cd88
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-03 11:16:34 +0530

    arrays in js

---
+++ 15.EverythingIsObject/arr.js
@@ -0,0 +1,35 @@
+let object = {
+	name: "Abhishek",
+	age: 22,
+	sayHi: function(){
+		console.log("Abhishek say's Hi");
+	}
+}
+
+// loop
+// for(let key in object){
+// 	console.log(key, " : ", object[key]);
+// }
+
+console.log("______________________________________________")
+
+const a=10;
+const arr = [1,2,3,4,5];
+// How is this possible ?
+// arr.shift();
+// console.log(arr);
+
+//console.log("______________________________________________")
+
+// In javascript arrays are not there.They are actually object only.
+arr.myprop = "Hello";
+//console.log(arr);
+arr.print = function(){
+	console.log("print function called");
+}
+//console.log(arr);
+
+// loop
+for(let key in arr){
+	console.log(key, " : ", arr[key]);
+}
