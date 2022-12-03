0243c88cf563639ad3ca5f1d1e1da91e1d9978bc
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-03 23:01:00 +0530

    Custom Filter Function

--- 18.Polyfills/pollyfills.js
+++ 18.Polyfills/pollyfills.js
@@ -1,28 +1,63 @@
-// Map
+// // Map
 
-let myArr = [1,2,3,4,5];
-let squaredArr = myArr.map(function(x){
-	return x*x;
+// let myArr = [1,2,3,4,5];
+// let squaredArr = myArr.map(function(x){
+// 	return x*x;
+// })
+
+// console.log(myArr);
+// console.log(squaredArr);
+
+// // Custom Map
+
+// function myPolyFillMap(arr,cb){
+// 	let newArr = [];
+
+// 	for(let i=0;i<arr.length;i++){
+// 		newArr.push(cb(arr[i]));
+// 	}
+
+// 	return newArr;
+// }
+
+// function square(x){
+// 	return x*x;
+// }
+
+// let squaredPolyfillArr = myPolyFillMap(myArr,square);
+// console.log(squaredPolyfillArr);
+
+
+console.log("__________________________________________________________________");
+
+
+// Filter
+
+let farr = [1,2,3,4,5];
+let newfarr = farr.filter(function(x){
+	return x % 2 === 0;
 })
 
-console.log(myArr);
-console.log(squaredArr);
+console.log(farr);
+console.log(newfarr);
 
-// Custom Map
+function myPolyfillFilter(arr,cb){
 
-function myPolyFillMap(arr,cb){
-	let newArr = [];
-
+	let filteredArr = [];
 	for(let i=0;i<arr.length;i++){
-		newArr.push(cb(arr[i]));
+		if(cb(arr[i])){
+			filteredArr.push(arr[i]);
+		}
 	}
 
-	return newArr;
+	return filteredArr;
 }
 
-function square(x){
-	return x*x;
+function filterEven(x){
+	if(x%2 === 0){
+		return x;
+	}	
 }
 
-let squaredPolyfillArr = myPolyFillMap(myArr,square);
-console.log(squaredPolyfillArr);
+let filteredPolyfillArr = myPolyfillFilter(farr,filterEven);
+console.log(filteredPolyfillArr);
