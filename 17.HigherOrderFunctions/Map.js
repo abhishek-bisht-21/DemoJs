51983cd6953a274ce9c6dac265fc47dca2fd86b8
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-03 13:39:05 +0530

    Map function

--- 17.HigherOrderFunctions/Map.js
+++ 17.HigherOrderFunctions/Map.js
@@ -1,5 +1,7 @@
 let myArr = [1,2,3,4,5];
 
+// map is a higher order function it takes function as an argument.
+// Map function one by one picks the elements in myArr and perform the desired activity/task on it.
 let newArr = myArr.map(function(x){
 	return x*x;
 });
