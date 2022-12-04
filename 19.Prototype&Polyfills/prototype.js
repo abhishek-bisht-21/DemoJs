9626f4d544f4f96b720d727e24b1da353fd3e821
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-04 09:37:14 +0530

    Prototype and Polyfills

--- 19.Prototype&Polyfills/prototype.js
+++ 19.Prototype&Polyfills/prototype.js
@@ -23,5 +23,24 @@ function square(x){
 
 
 let mappedArr = arr.myMap(square);
-
 console.log(mappedArr);
+
+console.log("_____________________________________________")
+
+Array .prototype.myFilter = function(cb){
+	let newArr = [];
+	for(let i=0;i<this.length;i++){
+		if(cb(this[i])){
+			newArr.push(this[i]);
+		}
+	}
+
+	return newArr;
+}
+
+function isEven(x){
+	return x%2===0?true:false
+}
+
+let filteredArr = arr.myFilter(isEven);
+console.log(filteredArr);
