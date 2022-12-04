c8020f3b45e84ad3a743d5d06ed34ee4cd666bab
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-04 09:31:27 +0530

    Prototype and Polyfills

---
+++ 19.Prototype&Polyfills/prototype.js
@@ -0,0 +1,26 @@
+Array.prototype.myFunction = function(){
+	console.log(this);
+}
+
+let arr = [1,2,3,4,5];
+
+arr.myFunction();
+
+Array.prototype.myMap = function(cb){
+	let newArr = [];
+
+	for(let i=0;i<this.length;i++){
+		newArr.push(cb(this[i]));
+	}
+
+	return newArr;
+}
+
+function square(x){
+	return x*x;
+}
+
+
+let mappedArr = arr.myMap(square);
+
+console.log(mappedArr);
